88b1fffec9d0be549471d9050fbfef642196e8ed
Witty <canis_lupus@adam.com.au>
2019-10-20 20:47:52 +1030

    Added pointer ref to other files and attempted to stop warnings from imgui

--- Dooda/src/Platform/OpenGL/OpenGLVertexArray.h
+++ Dooda/src/Platform/OpenGL/OpenGLVertexArray.h
@@ -13,16 +13,16 @@ namespace Dooda
 		virtual void Bind() const override;
 		virtual void UnBind() const override;
 
-		virtual void AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer) override;
-		virtual void SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer) override;
+		virtual void AddVertexBuffer(const Ref<VertexBuffer>& vertexBuffer) override;
+		virtual void SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer) override;
 
-		virtual const std::vector<std::shared_ptr<VertexBuffer>>& GetVertexBuffers() const override{ return d_vertexBuffers; }
-		virtual const std::shared_ptr<IndexBuffer>& GetIndexBuffer() const override { return d_indexBuffer; }
+		virtual const std::vector<Ref<VertexBuffer>>& GetVertexBuffers() const override{ return d_vertexBuffers; }
+		virtual const Ref<IndexBuffer>& GetIndexBuffer() const override { return d_indexBuffer; }
 
 	private:
 		uint32_t d_rendererID;
-		std::vector< std::shared_ptr<VertexBuffer>> d_vertexBuffers;
-		std::shared_ptr<IndexBuffer> d_indexBuffer;
+		std::vector<Ref<VertexBuffer>> d_vertexBuffers;
+		Ref<IndexBuffer> d_indexBuffer;
 	};
 
 }
